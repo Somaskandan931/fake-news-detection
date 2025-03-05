c5308d6c96e15ccdb30218ebc6115d5ff850f24b
Somaskandan931 <somaskandan931@gmail.com>
2025-03-05 12:48:03 +0530

    Update start.sh

--- Backend/start.sh
+++ Backend/start.sh
@@ -4,4 +4,4 @@ chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 echo "/swapfile none swap sw 0 0" >> /etc/fstab
-uvicorn main:app --host 0.0.0.0 --port $PORT --workers 1
+uvicorn Backend.main:app --host 0.0.0.0 --port $PORT --workers 1
